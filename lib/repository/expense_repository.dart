ca0369a7bf4702b49c47868fa96b8dc13728d72e
neamul-fahim <neamul.fahim@gmail.com>
2024-09-10 14:57:22 -0700

    small fix

--- lib/repository/expense_repository.dart
+++ lib/repository/expense_repository.dart
@@ -1,33 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:finance_management/models/expense.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class ExpenseRepository {
   
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final uid = '18DrbyyxN2f9hqmJ0vCBq6vHkzy2';
-
-  // Future<List<Expense>> getAllExpenses() async {
-  //   print('RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR');
-
-  //   final List<Expense> expenses = [];
-  //   final expensesRef = _firestore.collection("users").doc(uid).collection("expenses");
-
-  //   final snapshotStream = expensesRef.snapshots();
-
-  //   await for (var snapshot in snapshotStream) {
-  //     for (var doc in snapshot.docs) {
-  //       expenses.add(Expense.fromMap(doc));
-  //     }
-  //   }
-  //   print('RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR');
-  //   // print(expenses);
-
-  //   return expenses;
-  // }
 
   Future<List<Expense>> getAllExpenses(String userId) async {
-        print('RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR');
 
   final List<Expense> expenses = [];
 
@@ -43,7 +21,6 @@ class ExpenseRepository {
                   print(e.toString());
                   }                   
 
-  print(expenses);
   return expenses;
 }
 
